f34f6fc4446afa116e4815f40dbef20985e0451c
DanielVanDijkJacobsen <danilar@ruc.dk>
2021-03-14 21:19:03 +0100

    Change in formatting
    Adapted syntax to match python syntax

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ import wave
 
 def gather_sound(time, output_name):
     p = pyaudio.PyAudio()
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 44100
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
+    chunk = 1024
+    sound_format: int = pyaudio.paInt16
+    channels = 2
+    rate = 44100
+    stream = p.open(format=sound_format,
+                    channels=channels,
+                    rate=rate,
                     input=True,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=chunk)
     wave_output_filename = (output_name + ".wav")
     print("* recording")
     frames = []
-    for i in range(0, int((RATE / CHUNK) * time)):
-        data = stream.read(CHUNK)
+    for i in range(0, int((rate / chunk) * time)):
+        data = stream.read(chunk)
         frames.append(data)
     print("* done recording")
     stream.stop_stream()
     stream.close()
     p.terminate()
     wf = wave.open(wave_output_filename, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sound_format))
+    wf.setframerate(rate)
     wf.writeframes(b''.join(frames))
     wf.close()
 
